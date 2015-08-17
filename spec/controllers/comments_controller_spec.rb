b26f368cd90b5dcb207295686bbb6c15f7eab0c0
James <jlett89@gmail.comssh-keygen -t rsa -C jlett89@gmail.com>
2015-08-17 14:17:55 -0700

    created controller for comments, writing test for comments..failing for now

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe CommentsController, type: :controller do
+
+
+  describe 'POST #create' do
+		context 'with valid attributes' do
+			before(:each) do
+				@user = FactoryGirl.create :user
+				@post = FactoryGirl.create :post
+				sign_in @user
+				@comment_attributes = FactoryGirl.attributes_for :comment
+			end
+
+			it 'creates the comment' do
+				post :create, { user_id: @user.id, post_id: @post, comment: @comment_attributes }
+				expect(Comment.count).to eq(1)
+			end
+
+			it 'renders the comment' do
+				post :create, { user_id: @user.id, post_id: @post, comment: @comment_attributes }
+				expect(response).to redirect_to @post
+			end
+		end
+
+    context 'with invalid attributes' do
+      before(:each) do
+				@user = FactoryGirl.create :user
+				@post = FactoryGirl.create :post
+				sign_in @user
+				@invalid_comment_attributes = { body: nil }
+			end
+
+      it 'does not create the comment' do
+        post :create, { user_id: @user.id, post_id: @post, comment: @invalid_comment_attributes }
+        expect(Comment.count).to eq(0)
+      end
+    end
+	end
+end
