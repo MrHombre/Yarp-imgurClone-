660c88246e1ab4811b9675e94bdbfddd4e9fcee9
James <jlett89@gmail.comssh-keygen -t rsa -C jlett89@gmail.com>
2015-08-17 14:39:43 -0700

    render comments on show page/with any image, video shown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   validates :body, presence: true
+
+  auto_html_for :body do
+      html_escape
+      image
+      youtube(:width => 400, :height => 250, :autoplay => true)
+      link :target => "_blank", :rel => "nofollow"
+      simple_format
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,10 @@ class Post < ActiveRecord::Base
   validates :title, :image_url, :presence => true
 
   auto_html_for :image_url do
-    html_escape
-    image
-    youtube(:width => 400, :height => 250, :autoplay => true)
-    link :target => "_blank", :rel => "nofollow"
-    simple_format
-  end
+      html_escape
+      image
+      youtube(:width => 400, :height => 250, :autoplay => true)
+      link :target => "_blank", :rel => "nofollow"
+      simple_format
+    end
 end
