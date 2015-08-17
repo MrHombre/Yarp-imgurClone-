9850c01611c9ab58600121c293a53518c0da91a3
James <jlett89@gmail.comssh-keygen -t rsa -C jlett89@gmail.com>
2015-08-17 12:51:56 -0700

    add style to index, show and application pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,21 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+//flash message fade out
+
+$(document).ready(function(){
+  $(function() {
+   $('#flash').delay(500).slideDown('normal', function() {
+      $(this).delay(2500).slideUp();
+   });
+  });
+});
